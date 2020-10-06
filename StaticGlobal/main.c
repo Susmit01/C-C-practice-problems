2665045f9a74ffe7252287ba7ce2836a328409e2
Susmita Mishra <heysusmita@gmail.com>
2020-10-06 13:56:33 +0530

    N natural numbers

---
+++ StaticGlobal/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+int fun(int n){
+if (n>0)
+    {
+    return fun(n-1)+n;
+    }
+    return 0;
+}
+int main()
+{
+    int r;
+    r= fun(5);
+    printf("%d ", r);
+    return 0;
+}
