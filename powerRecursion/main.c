87384086fc446cbd976f9c8de820abf7b6b31546
Susmita Mishra <heysusmita@gmail.com>
2020-10-07 19:17:37 +0530

    Power of a number by saving time

--- powerRecursion/main.c
+++ powerRecursion/main.c
@@ -4,15 +4,24 @@
 int power(int m, int n)
 {
     if (n==0)
-    {
         return 1;
-    }
     return power(m, n-1)*m;
+}
+//more effective
+int ipower(int m, int n)
+{
+    if (n==0)
+        return 1;
+    if (n%2==0)
+        return ipower(m*m, n/2);
+    return m*ipower(m*m, (n-1)/2);
 }
 int main()
 {
     int r;
     r= power (2,4);
     printf("%d", r);
+    r= ipower(2,4);
+    printf("\n%d",r);
     return 0;
 }
