a4279333ebde56433915b4b7af9711f73662198d
Susmita Mishra <heysusmita@gmail.com>
2020-10-07 18:59:02 +0530

    factorial using loop

--- recursion/main.c
+++ recursion/main.c
@@ -1,16 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+//factorial
 int fact (int n)
 {
     if (n==0)
         return 1;
     return fact(n-1)*n;
 }
+//iteration
+int ifact(int n)
+{
+    int i, f=1;
+    for (i=1; i<=n; i++){
+        f=f*i;
+    }
+    return f;
+}
 int main()
 {
     int r;
     r= fact(5);
     printf("%d",r);
+    r= ifact(4);
+    printf("\n%d", r);
     return 0;
 }
