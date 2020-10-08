d24d0c311d9a27707dc471ed5cab0246f498301f
Susmita Mishra <heysusmita@gmail.com>
2020-10-08 12:11:20 +0530

    Taylor series using iteration

--- taylorSeries/main.c
+++ taylorSeries/main.c
@@ -22,9 +22,25 @@ double eHorner(int x, int n)
     s= 1 + x*s/n;
     return eHorner(x, n-1);
 }
+//iteratiom
+double eloop(int x, int n)
+{
+    double s=1;
+    int i;
+    double num=1;
+    double den=1;
+    for (i=1; i<=n;i++)
+    {
+        num*= x;
+        den*= i;
+        s+=num/den;
+    }
+    return s ;
+}
 int main()
 {
     printf("%lf \n", e(1,10));
     printf("%lf \n", eHorner(1, 10));
+    printf("%lf \n", eloop(1, 10));
     return 0;
 }
