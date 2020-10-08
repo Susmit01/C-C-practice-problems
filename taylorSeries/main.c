1b244d9fcccfb69488307470c98e9290a221b851
Susmita Mishra <heysusmita@gmail.com>
2020-10-08 12:08:17 +0530

    Taylor series using Horner's rule

--- taylorSeries/main.c
+++ taylorSeries/main.c
@@ -10,11 +10,21 @@ double e(int x, int n)
     r = e(x, n-1);
     p=p*x;
     f=f*n;
-    return r +p/f;
+    return r + p/f;
 
+}
+//horner's rule
+double eHorner(int x, int n)
+{
+    static double s;
+    if (n==0)
+        return s;
+    s= 1 + x*s/n;
+    return eHorner(x, n-1);
 }
 int main()
 {
     printf("%lf \n", e(1,10));
+    printf("%lf \n", eHorner(1, 10));
     return 0;
 }
