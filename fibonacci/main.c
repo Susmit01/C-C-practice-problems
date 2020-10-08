ffa57483f8be77176a83fad5eb3a965f5b3088f3
Susmita Mishra <heysusmita@gmail.com>
2020-10-08 13:05:45 +0530

    Fibonacci using memoization

--- fibonacci/main.c
+++ fibonacci/main.c
@@ -21,9 +21,34 @@ if (n<=1){
 }
 return rfib(n-1)+ rfib(n-2);
 }
+//memoization
+int f[10];
+int mfib (int n)
+{
+    if (n<=1)
+    {
+        f[n]=n;
+        return n;
+    }
+    else
+    {
+        if (f[n-2]==-1)
+            f[n-2]= mfib(n-2);
+        if (f[n-1] == -1)
+        {
+            f[n-1]= mfib(n-1);
+        }
+        return f[n-2] + f[n-1];
+    }
+
+}
 int main()
 {
+    int i;
+    for (i=0; i<10; i++)
+        f[i]= -1;
     printf("%d \n", fib(5));
     printf("%d \n", rfib(5));
+    printf("%d", mfib(5));
     return 0;
 }
