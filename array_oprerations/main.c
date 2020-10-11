c9b057d0c140e675204f576e2520c8b7fa091b07
Susmita Mishra <heysusmita@gmail.com>
2020-10-11 12:17:40 +0530

    insert at an index function

--- array_oprerations/main.c
+++ array_oprerations/main.c
@@ -19,11 +19,22 @@ void display (struct array arr)
     for (i=0; i<arr.len;i++)
         printf(" %d ",arr.a[i]);
 }
-
+void insert (struct array *arr, int index, int x)
+{
+    int i;
+    if (index>=0 && index<= arr->len)
+    {
+        for( i=arr->len; i>index;i--)
+            arr->a[i]=arr->a[i-1];
+        arr->a[index]=x;
+        arr->len++;
+    }
+}
 int main()
 {
     struct array arr={{2, 3, 4,5, 6}, 10, 5};
-    append(&arr, 10);
+    //append(&arr, 10);
+    insert(&arr, 5, 4);
     display(arr);
     return 0;
 }
